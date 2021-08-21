d85d53c1b36ab3d3da05bd8d6267de3e8591059a
David Fuelling <sappenin@gmail.com>
2021-08-21 14:58:42 -0600

    Fix Shard Computation
    Use LittleEndian instead of BE.
    
    Signed-off-by: David Fuelling <sappenin@gmail.com>

--- fastpay-core/src/main/java/com/sappenin/fastpay/core/AuthorityUtils.java
+++ fastpay-core/src/main/java/com/sappenin/fastpay/core/AuthorityUtils.java
@@ -1,6 +1,9 @@
 package com.sappenin.fastpay.core;
 
+import com.google.common.primitives.UnsignedInteger;
+
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 // TODO: Consider renaming to AuthorityStateUtils?
 public class AuthorityUtils {
@@ -11,8 +14,14 @@ public class AuthorityUtils {
   // TODO: Move to AuthorityState?
   public static int deriveShardNumber(final int numTotalShards, final FastPayAddress fastPayAddress) {
     // The last integer in the address is the last 4 bytes (32 bits).
-    final int num = ByteBuffer.wrap(fastPayAddress.edPublicKey().bytes(), LAST_INTEGER_INDEX, 4).getInt();
-    return num % numTotalShards;
+    int num = ByteBuffer
+      .wrap(fastPayAddress.edPublicKey().bytes(), LAST_INTEGER_INDEX, 4)
+      .order(ByteOrder.LITTLE_ENDIAN)
+      .getInt();
+    long numLong = num & 0XFFFFFFFFL; // Truncate any leading bits > 32
+    return UnsignedInteger.valueOf(numLong)
+      .mod(UnsignedInteger.valueOf(numTotalShards))
+      .intValue();
   }
 
 }
--- fastpay-core/src/test/java/com/sappenin/fastpay/core/AuthorityUtilsTest.java
+++ fastpay-core/src/test/java/com/sappenin/fastpay/core/AuthorityUtilsTest.java
@@ -11,8 +11,47 @@ class AuthorityUtilsTest {
 
   @Test
   void deriveShardNumber() {
-    int shardNum = AuthorityUtils.deriveShardNumber(4,
-      FastPayAddress.fromEd25519PublicKeyBase64("AzKoFE7qtEvQk+GHKt1YFJj28Pvk56rjV2T+fLowB+E="));
-    assertThat(shardNum).isEqualTo(0);
+
+    assertThat(
+      AuthorityUtils.deriveShardNumber(
+        4, FastPayAddress.fromEd25519PublicKeyBase64("A2cEntd9tcAMLsgayRNl6owrMOI5gEFtw1VoDayGAHg=")
+      )
+    ).isEqualTo(0);
+
+    assertThat(
+      AuthorityUtils.deriveShardNumber(
+        4, FastPayAddress.fromEd25519PublicKeyBase64("GCRtmYafSWOnYHDh9YukgfN2KSYaIe4YDFAVW9VXt+4=")
+      )
+    ).isEqualTo(1);
+
+    assertThat(
+      AuthorityUtils.deriveShardNumber(
+        4, FastPayAddress.fromEd25519PublicKeyBase64("AzKoFE7qtEvQk+GHKt1YFJj28Pvk56rjV2T+fLowB+E=")
+      )
+    ).isEqualTo(2);
+
+    assertThat(
+      AuthorityUtils.deriveShardNumber(
+        4, FastPayAddress.fromEd25519PublicKeyBase64("BWR/Bvn4VWTsuLKqtLVPT7Q3yEJzKXyC/1GIFJ/AHLw=")
+      )
+    ).isEqualTo(3);
+
+    assertThat(
+      AuthorityUtils.deriveShardNumber(
+        4, FastPayAddress.fromEd25519PublicKeyBase64("Cm8jsuewLGo/YWGRMEzzyVdzxzEkOMwwvmXxkmqykY0=")
+      )
+    ).isEqualTo(2);
+
+    assertThat(
+      AuthorityUtils.deriveShardNumber(
+        4, FastPayAddress.fromEd25519PublicKeyBase64("DYQIqYnnYpHcl3xd6MYfmpJiWxd6I2VSUSyi2itvm34=")
+      )
+    ).isEqualTo(3);
+
+    assertThat(
+      AuthorityUtils.deriveShardNumber(
+        4, FastPayAddress.fromEd25519PublicKeyBase64("EUmqG4EBBq/A7SJwTure9BOfTs5HqBPWw57QOUtFXsg=")
+      )
+    ).isEqualTo(3);
   }
 }
