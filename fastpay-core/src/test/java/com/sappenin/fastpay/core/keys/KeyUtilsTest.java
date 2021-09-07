85a2f62105191acc61f04ba960c44738c7250d31
Danil Nemirovsky <danil.nemirovsky@gmail.com>
2021-09-07 18:35:30 +0100

    Add serialisation to FastPay key pair

--- fastpay-core/src/test/java/com/sappenin/fastpay/core/keys/KeyUtilsTest.java
+++ fastpay-core/src/test/java/com/sappenin/fastpay/core/keys/KeyUtilsTest.java
@@ -3,12 +3,15 @@ package com.sappenin.fastpay.core.keys;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.io.BaseEncoding;
+import java.util.stream.Stream;
 import org.bouncycastle.crypto.params.Ed25519PrivateKeyParameters;
 import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
 import org.junit.jupiter.api.Test;
 
 import javax.security.auth.DestroyFailedException;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 /**
@@ -27,7 +30,12 @@ class KeyUtilsTest {
   private static final String FP_PUBLIC_KEY2_B64 = "A2cEntd9tcAMLsgayRNl6owrMOI5gEFtw1VoDayGAHg=";
 
   private static final String FP_PRIVATE_KEY1_B64 = "hPDiMD6fD3PNKH8SsEkGiETzmoGudNtNi3zuFGRkuecDMqgUTuq0S9CT4Ycq3VgUmPbw++TnquNXZP58ujAH4Q==";
+  private static final byte[] PRIVATE_KEY1_BYTES = {-124, -16, -30, 48, 62, -97, 15, 115, -51, 40, 127, 18, -80, 73, 6, -120, 68, -13, -102, -127, -82, 116, -37, 77, -117, 124, -18, 20, 100, 100, -71, -25};
+  private static final byte[] PUBLIC_KEY1_BYTES = {3, 50, -88, 20, 78, -22, -76, 75, -48, -109, -31, -121, 42, -35, 88, 20, -104, -10, -16, -5, -28, -25, -86, -29, 87, 100, -2, 124, -70, 48, 7, -31};
+
   private static final String FP_PRIVATE_KEY2_B64 = "rcc3gEhS6sXf/0sic9DionRoh2D/hjmw0EhFJ9VpyBwDZwSe1321wAwuyBrJE2XqjCsw4jmAQW3DVWgNrIYAeA==";
+  private static final byte[] PRIVATE_KEY2_BYTES = {-83, -57, 55, -128, 72, 82, -22, -59, -33, -1, 75, 34, 115, -48, -30, -94, 116, 104, -121, 96, -1, -122, 57, -80, -48, 72, 69, 39, -43, 105, -56, 28};
+  private static final byte[] PUBLIC_KEY2_BYTES = {3, 103, 4, -98, -41, 125, -75, -64, 12, 46, -56, 26, -55, 19, 101, -22, -116, 43, 48, -30, 57, -128, 65, 109, -61, 85, 104, 13, -84, -122, 0, 120};
 
   @Test
   void edPrivateKeyParametersToPrivateKeyAndBack() {
@@ -133,5 +141,22 @@ class KeyUtilsTest {
     final KeyPair keyPair = KeyUtils.fromFastpaySecretKeyB64(key);
     assertThat(keyPair.publicKey()).isEqualTo(expectedPublicKey);
     assertThat(keyPair.privateKey()).isEqualTo(expectedPrivateKey);
+  }
+
+  @ParameterizedTest
+  @MethodSource("providePrivateAndPublicKeyBytes")
+  void toFastPaySecretKey1(final byte[] privateKey, final byte[] publicKey, final String expected) {
+    final Ed25519PrivateKey expectedPrivateKey = Ed25519PrivateKey.of(privateKey);
+    final Ed25519PublicKey expectedPublicKey = Ed25519PublicKey.of(publicKey);
+
+    final String keyPair = KeyUtils.toFastPaySecretKeyB64(expectedPrivateKey, expectedPublicKey);
+    assertThat(keyPair).isEqualTo(expected);
+  }
+
+  private static Stream<Arguments> providePrivateAndPublicKeyBytes() {
+    return Stream.of(
+        Arguments.of(PRIVATE_KEY1_BYTES, PUBLIC_KEY1_BYTES, FP_PRIVATE_KEY1_B64),
+        Arguments.of(PRIVATE_KEY2_BYTES, PUBLIC_KEY2_BYTES, FP_PRIVATE_KEY2_B64)
+    );
   }
 }
